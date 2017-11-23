37da9e4c1ffa74067563692d2879e2563be6e3d9
bifjhh <929160004@qq.com>
2017-11-23 13:58:01 +0800

    修改新闻列表页

--- src/component/news/newslist.vue
+++ src/component/news/newslist.vue
@@ -4,7 +4,6 @@
             <router-link to="/home" slot="left">
                 <mt-button icon="back">返回</mt-button>
             </router-link>
-            <mt-button icon="more" slot="right"></mt-button>
         </mt-header>
        <ul class="mui-table-view">
             <li class="mui-table-view-cell mui-media" v-for="v in list">
