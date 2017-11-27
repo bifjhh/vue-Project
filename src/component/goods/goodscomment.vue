81a4ffe70e0642a08eff2924bdc8367d83bbe2ad
bifjhh <929160004@qq.com>
2017-11-27 18:09:44 +0800

    商品评论

--- src/component/goods/goodscomment.vue
+++ src/component/goods/goodscomment.vue
@@ -1,10 +1,6 @@
 <template>
   <div class="temp">
-      <mt-header fixed title="发表评论">
-            <router-link v-bind="{to:'/goods/goodsinfo/'+id}" slot="left">
-                <mt-button icon="back">返回</mt-button>
-            </router-link>
-        </mt-header>
+      <mt-header fixed title="发表评论"></mt-header>
       <comment :id="id"></comment>
   </div>
 </template>
