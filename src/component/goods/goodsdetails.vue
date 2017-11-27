7dcfd650dba9e01c9767c374fb43ced180dde7e2
bifjhh <929160004@qq.com>
2017-11-27 18:13:08 +0800

    商品详情

--- src/component/goods/goodsdetails.vue
+++ src/component/goods/goodsdetails.vue
@@ -1,10 +1,6 @@
 <template>
     <div class="temp">
-        <mt-header fixed :title="info.title">
-            <router-link v-bind="{to:'/goods/goodsinfo/'+id}" slot="left">
-                <mt-button icon="back">返回</mt-button>
-            </router-link>
-        </mt-header>
+        <mt-header fixed :title="info.title"></mt-header>
         <h4 v-text="info.title"></h4>
         <hr>
         <div class="content">
