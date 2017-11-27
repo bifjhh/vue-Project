6dd7fc920043b2d4c132cbe3f30a0ad24018bcce
bifjhh <929160004@qq.com>
2017-11-27 18:08:20 +0800

    图片列表页面

--- src/component/photo/photolist.vue
+++ src/component/photo/photolist.vue
@@ -1,11 +1,6 @@
 <template>
     <div class="temp">
-        <mt-header fixed title="图片分享">
-            <router-link to="/home" slot="left">
-                <mt-button icon="back">返回</mt-button>
-            </router-link>
-            <mt-button icon="more" slot="right"></mt-button>
-        </mt-header>
+        <mt-header fixed title="图片分享"></mt-header>
         <!-- 图片分类 -->
         <div class="classify">
             <ul v-bind="{style : 'width:' + ulWidth + 'px'}">
