9511a1197da2af60727db86afc72409b420f83a4
bifjhh <929160004@qq.com>
2017-11-27 18:10:21 +0800

    商品详情页面

--- src/component/goods/goodsinfo.vue
+++ src/component/goods/goodsinfo.vue
@@ -1,10 +1,6 @@
 <template>
   <div class="temp">
-        <mt-header fixed :title="info.title">
-            <router-link to="/goods/goodslist" slot="left">
-                <mt-button icon="back">返回</mt-button>
-            </router-link>
-        </mt-header>
+        <mt-header fixed :title="info.title"></mt-header>
         <!-- 轮播图 -->
         <swipe :imgs="imgs"></swipe>  
         <div class="price">
